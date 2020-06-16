3589dcbbbe322511e7f98a4a84721bff140d36df
Tam Pham <tamphamnguyen92@gmail.com>
2020-06-16 19:32:47 +0800

    added admin styling

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+const showStudentDropdown = () => {
+    $('dropdown-container-students').css('display', 'block');
+}
+
+const showTeacherDropdown = () => {
+    $('dropdown-container-teachers').css('display', 'block');
+}
