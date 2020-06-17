b97c32bd47b2dcd6dc1c48752805882db50e9307
Tam Pham <tamphamnguyen92@gmail.com>
2020-06-17 22:23:28 +0800

    added new form

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,21 @@
 const showStudentDropdown = () => {
     $('.dropdown-container-students').css('display', 'block');
-}
+};
 
 const showTeacherDropdown = () => {
     $('.dropdown-container-teachers').css('display', 'block');
-}
+};
 
 const showSubTypeDropdown = () => {
     $('.dropdown-container').css('display', 'none');
     const id = parseInt(event.currentTarget.id.replace('dropdown-',''));
     $(`#dropdown-container-${id}`).css('display', 'block');
+};
+
+const launchChildRegistrationForm = () => {
+    $('#child-registration-form').css('display', 'flex');
+};
+
+const closeFormModal = () => {
+    $('#child-registration-form').css('display', 'none');
 }
