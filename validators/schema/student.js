64a680ab69ebfc9c80d0f6d59169e5c9bba36414
Tam Pham <tamphamnguyen92@gmail.com>
2020-06-17 23:25:53 +0800

    added validator

---
+++ validators/schema/student.js
@@ -0,0 +1,38 @@
+module.exports = {
+    type: 'object',
+    required: ['firstName', 'ageGroup', 'guardianName', 'guardianContact'],
+    properties: {
+        firstName: {
+            type: 'string',
+            minLength: 2
+        },
+        lastName: {
+            type: 'string',
+        },
+        dob: {
+            type: 'string',
+            format: 'date-time'
+        },
+        ageGroup: {
+            type: 'string',
+        },
+        guardianName: {
+            type: 'string',
+            minLength: 2
+        },
+        guardianContact: {
+            type: 'string',
+            minLength: 5
+        },
+        guardianRole: {
+            type: 'string'
+        },
+        guardianMembership: {
+            type: 'string',
+        },
+        firstSeen: {
+            type: 'string',
+            format: 'date-time'
+        }
+    }
+}
