d67adf66ddfded3d5d48af7b015420b6524062ea
Tam Pham <tamphamnguyen92@gmail.com>
2020-06-19 09:49:55 +0800

    added get routes for teacher-age groups

--- controllers/teacherControllers.js
+++ controllers/teacherControllers.js
@@ -6,5 +6,53 @@ module.exports = {
         res.render('admin-teacher-ageGroups.ejs', {
             data: teacherData
         });
+    },
+    async showTotsData (req, res) {
+        const teacherData = await services.teacherService.getTeachersByAgeGroup('Tots');
+        res.render('admin-teacher-ageGroups.ejs', {
+            data: teacherData
+        });
+    },
+    async showJuniorData (req, res) {
+        const teacherData = await services.teacherService.getTeachersByAgeGroup('Junior');
+        res.render('admin-teacher-ageGroups.ejs', {
+            data: teacherData
+        });
+    },
+    async showLowerPrimaryData (req, res) {
+        const teacherData = await services.teacherService.getTeachersByAgeGroup('Lower Primary');
+        res.render('admin-teacher-ageGroups.ejs', {
+            data: teacherData
+        });
+    },
+    async showUpperPrimaryData (req, res) {
+        const teacherData = await services.teacherService.getTeachersByAgeGroup('Upper Primary');
+        res.render('admin-teacher-ageGroups.ejs', {
+            data: teacherData
+        });
+    },
+    async addNewTeacherData (req, res) {
+        const studentDetails = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            dob: req.body.dob,
+            ageGroup: req.body.ageGroup,
+            guardianName: req.body.guardianName,
+            guardianContact: req.body.guardianContact,
+            guardianRole: req.body.guardianRole,
+            membership: req.body.membership,
+            firstSeen: req.body.firstSeen,
+        }
+        try {
+            studentValidator.students.validate(studentDetails);
+            const childrenData = await services.studentService.createNewStudent(studentDetails);
+            res.redirect('/students/age');
+        } catch (err) {
+            const childrenData = await services.studentService.getStudentsByAgeGroup('Tots');
+            res.render('admin-newChild.ejs', {
+                data: childrenData,
+                err
+            })    
+        }
     }
 }
--- router/routes.js
+++ router/routes.js
@@ -12,13 +12,19 @@ router.post('/login', controllers.onboardingControllers.verifyAccount);
 router.post('/', controllers.onboardingControllers.createNewUser);
 router.put('/dashboard', controllers.userProfileControllers.editUserProfile);
 
-//Super Admin App Routes
+//Super Admin App Routes - Students
 router.get('/students/age', controllers.studentControllers.showDefaultData)
 router.get('/students/age/tots', controllers.studentControllers.showTotsData)
 router.get('/students/age/junior', controllers.studentControllers.showJuniorData)
 router.get('/students/age/lower-primary', controllers.studentControllers.showLowerPrimaryData)
 router.get('/students/age/upper-primary', controllers.studentControllers.showUpperPrimaryData)
 router.post('/students/age', controllers.studentControllers.addNewStudentData)
+
+//Super Admin App Routes - Teachers
 router.get('/teachers/age', controllers.teacherControllers.showDefaultData)
+router.get('/teachers/age/tots', controllers.teacherControllers.showTotsData)
+router.get('/teachers/age/junior', controllers.teacherControllers.showJuniorData)
+router.get('/teachers/age/lower-primary', controllers.teacherControllers.showLowerPrimaryData)
+router.get('/teachers/age/upper-primary', controllers.teacherControllers.showUpperPrimaryData)
 
 module.exports = router;
